4ee9416b0273173402b44e4d99e79a277f21b229
Greet Verduyn <greetverduyn@proximus.be>
2022-06-17 11:01:40 +0200

    exercise 1 done

--- expert.php
+++ expert.php
@@ -2,11 +2,33 @@
 declare(strict_types=1);
 
 
-// exercise 1
-function new_exercise() {
+// === Exercise 1 ===
+// Below we're defining a function, but it doesn't work when we run it.
+// Look at the error you get, read it and it should tell you the issue...,
+// sometimes, even your IDE can tell you what's wrong
+
+echo "Exercise 1 starts here:";
+
+function new_exercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
 
 }
 
+new_exercise(2);
 
+/*
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[1];
+
+echo $monday;
+
+
+
+
+
+*/
